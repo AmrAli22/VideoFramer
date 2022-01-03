2c1f366675df569781137b8c69539aa520ee9a8f
AmrAli22 <amr.alifciscu@gmail.com>
2022-01-03 22:26:41 +0200

    Import Video and get video URL

--- VideoFramer/ViewController.swift
+++ VideoFramer/ViewController.swift
@@ -6,20 +6,32 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AVFoundation
+import AssetsLibrary
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let imagePickerController = UIImagePickerController()
+    var videoURL : NSURL?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func importVedioBtnPressed(_ sender: Any) {
+        imagePickerController.sourceType = .savedPhotosAlbum
+        imagePickerController.delegate = self
+        imagePickerController.mediaTypes = [kUTTypeMovie as String]
+        present(imagePickerController, animated: true, completion: nil)
     }
-    
-    
-    
-
 }
 
+extension ViewController {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? NSURL
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+}
