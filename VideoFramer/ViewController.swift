01f93dc85a2af35e1df84fecbdecf5ac3631d66f
AmrAli22 <amr.alifciscu@gmail.com>
2022-01-03 21:48:58 +0200

    Build Required UI

--- VideoFramer/ViewController.swift
+++ VideoFramer/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func importVedioBtnPressed(_ sender: Any) {
+    }
+    
+    
+    
 
 }
 
